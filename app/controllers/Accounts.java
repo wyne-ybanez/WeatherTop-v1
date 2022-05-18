f87d79cf8273a7fcd1cc34bf53bfb818d41d063e
wyne-ybanez <wyne_01@icloud.com>
2022-05-18 23:08:19 +0100

    Add authentication process for logins

--- app/controllers/Accounts.java
+++ app/controllers/Accounts.java
@@ -23,4 +23,32 @@ public class Accounts extends Controller
     public static void login(){
         render("login.html");
     }
+
+    public static void logout(){
+        session.clear();
+        redirect("/");
+    }
+
+    /**
+     * User Authentication
+     * Finds member through the user email.
+     * Adds memberId to the session should authentication be successful.
+     *
+     * @param email
+     * @param password
+     */
+    public static void authenticate(String email, String password){
+        Logger.info("Attempting to authenticate with " + email + ":" + password);
+
+        Member member = Member.findByEmail(email);
+        if ((member != null) && (member.checkPassword(password) == true)){
+            Logger.info("Authentication successful");
+            session.put("logged_in_Memberid", member.id);
+            redirect("/dashboard");
+        }
+        else {
+            Logger.info("Authentication failed");
+            redirect("/login");
+        }
+    }
 }
